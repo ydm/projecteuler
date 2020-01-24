1e410e0a647e645b20810ec45dcd66405342e109
Yordan Miladinov <yordan.miladinov@chaosgroup.com>
2020-01-24 10:40:31 -0800

    a bit smarter

--- pe024/src/Lib.hs
+++ pe024/src/Lib.hs
@@ -1,7 +1,4 @@
-module Lib
-  ( factorials
-  , perm
-  ) where
+module Lib where
 
 -- |Return the reversed list of all factorials mapped over the the
 --  range [0; n-1]
@@ -9,22 +6,23 @@ module Lib
 factorials :: (Num a, Enum a) => a -> [a]
 factorials n = scanr (*) 1 [n-1, n-2 .. 1]
 
--- |Remove an element by index from the given list
-pop :: Int -> [a] -> [a]
-pop i xs = (take i xs) ++ (drop (i + 1) xs)
+remainders :: Integral a => a -> a -> [a]
+remainders index len = scanl rem index $ factorials len
 
--- |Return the nth permutation of xs
+indices :: Integral a => a -> a -> [a]
+indices index len = zipWith quot (remainders index len) (factorials len)
+
+-- |Remove an element by index from the given list and return a tuple
+--  of the removed element and the modified list
+pop :: Int -> [a] -> (a, [a])
+pop i xs = (pivot, left ++ right)
+  where left = take i xs
+        pivot:right = drop i xs
+
+populate :: [a] -> [Int] -> [a]
+populate _ [] = []
+populate xs (i:is) = y : populate ys is
+  where (y, ys) = pop i xs
+
 perm :: Int -> [a] -> [a]
-perm index xs = let (_, _, ys) = perm' index xs in reverse ys
-
--- The memo in f is made up of the following triplet:
---   * remainder of the index
---   * elements left to pick from
---   * picked elements
-perm' :: Int -> [a] -> (Int, [a], [a])
-perm' index xs = foldl f (index, xs, []) (factorials $ length xs)
-  where f (n, elements, picked) fact =
-          let q = n `quot` fact -- index/factorial quotient
-              r = n `rem` fact  -- index/factorial remainder
-              p = elements !! q -- picked element
-          in (r, pop q elements, p:picked)
+perm index xs = populate xs $ indices index $ length xs
